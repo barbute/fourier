5c2e64d1671fe338dfaef0d31c434e9af2f62efd
barbute <endman3040@gmail.com>
2024-03-22 13:21:12 -0500

    Added setpoints, setters, and log inputs for Indexer - runs in periodic

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -4,11 +4,73 @@
 
 package frc.robot.subsystems.indexer;
 
+import java.util.function.DoubleSupplier;
+
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.debugging.LoggedTunableNumber;
 
 public class Indexer extends SubsystemBase {
-  public Indexer() {}
+  public enum IndexerSetpoints {
+    INTAKE(() -> 12.0),
+    STOW(() -> 4.0),
+    OUTTAKE(() -> -12.0),
+    SPEAKER_SHOOT(() -> 12.0),
+    AMP_SHOOT(() -> 7.0),
+    CUSTOM(new LoggedTunableNumber("Indexer/VoltageSetpoint", 4.0)),
+    STOPPED(() -> 0.0);
+
+    private DoubleSupplier setpointVolts;
+
+    IndexerSetpoints(DoubleSupplier volts) {
+      this.setpointVolts = volts;
+    }
+
+    public double getVolts() {
+      return this.setpointVolts.getAsDouble();
+    }
+  }
+
+  private IndexerSetpoints currentSetpoint = IndexerSetpoints.STOPPED;
+
+  private IndexerIO indexerIO;
+  private IndexerIOInputsAutoLogged indexerIOInputs = new IndexerIOInputsAutoLogged();
+
+  public Indexer(IndexerIO io) {
+    indexerIO = io;
+  }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    indexerIO.updateInputs(indexerIOInputs);
+    Logger.processInputs("Indexer", indexerIOInputs);
+
+    if (DriverStation.isDisabled()) {
+      currentSetpoint = IndexerSetpoints.STOPPED;
+      indexerIO.setVolts(0.0);
+    }
+
+    if (currentSetpoint != null) {
+      indexerIO.setVolts(currentSetpoint.getVolts());
+    }
+  }
+
+  /** Run the indexer at a setpoint voltage */
+  public void runIndexer(IndexerSetpoints setpoint) {
+    currentSetpoint = setpoint;
+  }
+
+  /** Returns the current setpoint state of the Indexer */
+  @AutoLogOutput(key = "Indexer/CurrentSetpoint")
+  public IndexerSetpoints getCurrentSetpoint() {
+    if (currentSetpoint != null) {
+      return currentSetpoint;
+    }
+    else {
+      return IndexerSetpoints.STOPPED;
+    }
+  }
 }
