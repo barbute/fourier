b88eab74c4feb10475dccb587f75fdfc99e78c35
barbute <endman3040@gmail.com>
2024-03-22 12:47:42 -0500

    Base files for Indexer

---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOSim.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.indexer;
+
+/** Physics sim implementation of IndexerIO */
+public class IndexerIOSim implements IndexerIO {
+
+  @Override
+  public void updateInputs(IndexerIOInputs inputs) {}
+
+  @Override
+  public void setVolts(double volts) {}
+}
---
+++ src/main/java/frc/robot/subsystems/indexer/IndexerIOSparkMax.java
@@ -0,0 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.indexer;
+
+/** IndexerIO implementation for SparkMax motor controller (NEO) */
+public class IndexerIOSparkMax implements IndexerIO {
+
+  public IndexerIOSparkMax() {}
+
+  @Override
+  public void updateInputs(IndexerIOInputs inputs) {}
+
+  @Override
+  public void setVolts(double volts) {}
+}
