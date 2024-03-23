f83130ce00a7d5e705aba822eb20b98131b814a6
barbute <endman3040@gmail.com>
2024-03-23 08:58:41 -0500

    Hardware interface for Angler

--- src/main/java/frc/robot/subsystems/shooter/angler/AnglerIO.java
+++ src/main/java/frc/robot/subsystems/shooter/angler/AnglerIO.java
@@ -4,5 +4,23 @@
 
 package frc.robot.subsystems.shooter.angler;
 
-/** Add your docs here. */
-public interface AnglerIO {}
+import org.littletonrobotics.junction.AutoLog;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public interface AnglerIO {
+  @AutoLog
+  public static class AnglerIOInputs {
+    public Rotation2d position = new Rotation2d();
+    public double velocityRadsPerSec = 0.0;
+    public double appliedVolts = 0.0;
+    public double[] appliedCurrentAmps = new double[] {0.0};
+    public double[] temperatureCelsius = new double[] {0.0};
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(AnglerIOInputs inputs) {}
+
+  /** Run the angler motor at the specified voltage. */
+  public default void setVolts(double volts) {}
+}
