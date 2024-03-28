af0ba4269d0f7fcbdeeccaaca1613c7850d2a816
barbute <endman3040@gmail.com>
2024-03-28 18:23:09 -0500

    Hardware implementation of YoshiIO

--- src/main/java/frc/robot/subsystems/yoshi/YoshiIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/yoshi/YoshiIOSparkMax.java
@@ -4,16 +4,83 @@
 
 package frc.robot.subsystems.yoshi;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkLowLevel.PeriodicFrame;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
+
 /** YoshiIO implementation for two SparkMax motor controllers (NEO 550s) */
 public class YoshiIOSparkMax implements YoshiIO {
-  public YoshiIOSparkMax() {}
+  // private final double PIVOT_GEAR_RATIO = (64.0 / 1.0) * (3.0 / 1.0);
+  // private final double FLYWHEEL_GEAR_RATIO = 5.0 / 1.0;
+
+  private CANSparkMax pivotMotor = new CANSparkMax(52, MotorType.kBrushless);
+  private CANSparkMax flywheelMotor = new CANSparkMax(53, MotorType.kBrushless);
+
+  private DutyCycleEncoder pivotAbsoluteEncoder = new DutyCycleEncoder(8);
+
+  private Rotation2d pivotAbsoluteOffset = Rotation2d.fromDegrees(73.0);
+
+  public YoshiIOSparkMax() {
+    pivotMotor.clearFaults();
+    pivotMotor.restoreFactoryDefaults();
+
+    pivotMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 100);
+    pivotMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus1, 100);
+    pivotMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 100);
+    pivotMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 100);
+    pivotMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus4, 100);
+
+    pivotMotor.setSmartCurrentLimit(40);
+    pivotMotor.enableVoltageCompensation(12.0);
+    pivotMotor.setIdleMode(IdleMode.kBrake);
+
+    pivotMotor.setInverted(true);
+
+    pivotMotor.burnFlash();
+
+    pivotAbsoluteEncoder.setDutyCycleRange(1.0 / 8192.0, 8191.0 / 8192.0);
+
+    flywheelMotor.clearFaults();
+    flywheelMotor.restoreFactoryDefaults();
+
+    flywheelMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 100);
+    flywheelMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus0, 50);
+    flywheelMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus2, 100);
+    flywheelMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus3, 100);
+    flywheelMotor.setPeriodicFramePeriod(PeriodicFrame.kStatus4, 100);
+
+    flywheelMotor.setSmartCurrentLimit(60);
+    flywheelMotor.enableVoltageCompensation(12.0);
+    flywheelMotor.setIdleMode(IdleMode.kBrake);
+
+    flywheelMotor.setInverted(false);
+
+    flywheelMotor.burnFlash();
+  }
 
   @Override
-  public void updateInputs(YoshiIOInputs inputs) {}
+  public void updateInputs(YoshiIOInputs inputs) {
+    inputs.pivotPosition = Rotation2d.fromRotations(pivotAbsoluteEncoder.getAbsolutePosition()).minus(pivotAbsoluteOffset).times(-1.0);
+    inputs.pivotAppliedVolts = pivotMotor.getAppliedOutput() * pivotMotor.getBusVoltage();
+    inputs.pivotAppliedCurrentAmps = new double[] {pivotMotor.getOutputCurrent()};
+    inputs.pivotTemperatureCelsius = new double[] {pivotMotor.getMotorTemperature()};
+
+    inputs.flywheelAppliedVolts = flywheelMotor.getAppliedOutput() * pivotMotor.getBusVoltage();
+    inputs.flywheelAppliedCurrentAmps = new double[] {flywheelMotor.getOutputCurrent()};
+    inputs.flywheelTemperatureCelsius = new double[] {flywheelMotor.getMotorTemperature()};
+  }
 
   @Override
-  public void setPivotVolts(double volts) {}
+  public void setPivotVolts(double volts) {
+    pivotMotor.setVoltage(volts);
+  }
 
   @Override
-  public void setFlywheelVolts(double volts) {}
+  public void setFlywheelVolts(double volts) {
+    flywheelMotor.setVoltage(volts);
+  }
 }
