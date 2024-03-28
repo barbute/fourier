20e61410093b6ce351783d7c8d14b748f582c269
barbute <endman3040@gmail.com>
2024-03-27 20:13:18 -0500

    Base vision classes (whoops meant to do this on dif branch)

---
+++ src/main/java/frc/robot/subsystems/vision/VisionIO.java
@@ -0,0 +1,68 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import org.littletonrobotics.junction.AutoLog;
+
+public interface VisionIO {
+  @AutoLog
+  public static class VisionIOInputs {
+    public boolean isConnected = false;
+    public boolean hasTarget = false;
+    public boolean hasTargetDebounced = false;
+    public boolean hasSpeakerTarget = false;
+
+    public double latestTimestamp = 0.0;
+    public double latencySeconds = 0.0;
+    public int numberOfTargets = 0;
+    public int aprilTagID = 0;
+
+    public double yaw = 0.0;
+    public double pitch = 0.0;
+    public double area = 0.0;
+
+    public Transform3d cameraToApriltag = new Transform3d();
+    public Transform3d robotToApriltag = new Transform3d();
+    public Transform2d speakerTagTransform = new Transform2d();
+    public Pose2d speakerTagPose = new Pose2d();
+    public Pose2d estimatedRobotPose = new Pose2d();
+
+    public double poseAmbiguity = 0.0;
+
+    public double xStandardDeviation = 0.0;
+    public double yStandardDeviation = 0.0;
+    public double thetaStandardDeviation = 0.0;
+
+    public double speakerXStdDev = 0.0;
+    public double speakerYStdDev = 0.0;
+    public double speakerThetaDev = 0.0;
+  }
+
+  /** Updates the set of loggable inputs. */
+  public default void updateInputs(VisionIOInputs inputs) {}
+
+  /** Sets the standard deviation for single-tag detection */
+  public default void setSingleTagStdDevs(double x, double y, double theta) {}
+
+  /** Sets the standard deviation for multi-tag detection */
+  public default void setMultiTagStdDevs(double x, double y, double theta) {}
+
+  /** Returns the standard deviation for single-tag detection */
+  public default Matrix<N3, N1> getSingleTagStdDevsCoeff() {
+    return VecBuilder.fill(0.0, 0.0, 0.0);
+  }
+
+  /** Returns the standard deviation for multi-tag detection */
+  public default Matrix<N3, N1> getMultiTagStdDevsCoeff() {
+    return VecBuilder.fill(0.0, 0.0, 0.0);
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/vision/VisionIOSim.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.vision;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+
+/** VisionIO implementation for using Photon Vision on a simulated camera */
+public class VisionIOSim implements VisionIO {
+
+  public VisionIOSim() {}
+
+  @Override
+  public void updateInputs(VisionIOInputs inputs) {}
+
+  @Override
+  public void setSingleTagStdDevs(double x, double y, double theta) {}
+
+  @Override
+  public void setMultiTagStdDevs(double x, double y, double theta) {}
+
+  @Override
+  public Matrix<N3, N1> getSingleTagStdDevsCoeff() {
+    return VecBuilder.fill(0.0, 0.0, 0.0);
+  }
+
+  @Override
+  public Matrix<N3, N1> getMultiTagStdDevsCoeff() {
+    return VecBuilder.fill(0.0, 0.0, 0.0);
+  }
+}
