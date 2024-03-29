c60e4a462f00a0d14b0e89c574daabe3883f77fa
barbute <endman3040@gmail.com>
2024-03-28 20:39:23 -0500

    Added tunables for StdDevs

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -5,13 +5,86 @@
 package frc.robot.subsystems.vision;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.util.debugging.LoggedTunableNumber;
+import org.littletonrobotics.junction.Logger;
 
 public class Vision extends SubsystemBase {
-  /** Creates a new Vision. */
-  public Vision() {}
+  // Relative to facing the same direction as the shooter barrel
+  private VisionIO frontLeftIO;
+  private VisionIO frontRightIO;
+
+  private VisionIOInputsAutoLogged frontLeftIOInputs = new VisionIOInputsAutoLogged();
+  private VisionIOInputsAutoLogged frontRightIOInputs = new VisionIOInputsAutoLogged();
+
+  private LoggedTunableNumber singleTagStdDevX;
+  private LoggedTunableNumber singleTagStdDevY;
+  private LoggedTunableNumber singleTagStdDevTheta;
+
+  private LoggedTunableNumber multiTagStdDevX;
+  private LoggedTunableNumber multiTagStdDevY;
+  private LoggedTunableNumber multiTagStdDevTheta;
+
+  public Vision(VisionIO frontLeftIO, VisionIO frontRightIO) {
+    this.frontLeftIO = frontLeftIO;
+    this.frontRightIO = frontRightIO;
+
+    singleTagStdDevX =
+        new LoggedTunableNumber(
+            "Vision/SingleTagDevsX", this.frontLeftIO.getSingleTagStdDevsCoeff().get(0, 0));
+    singleTagStdDevY =
+        new LoggedTunableNumber(
+            "Vision/SingleTagDevsY", this.frontLeftIO.getSingleTagStdDevsCoeff().get(1, 0));
+    singleTagStdDevTheta =
+        new LoggedTunableNumber(
+            "Vision/SingleTagDevsTheta", this.frontLeftIO.getSingleTagStdDevsCoeff().get(2, 0));
+
+    multiTagStdDevX =
+        new LoggedTunableNumber(
+            "Vision/MultiTagDevsX", this.frontLeftIO.getMultiTagStdDevsCoeff().get(0, 0));
+    multiTagStdDevY =
+        new LoggedTunableNumber(
+            "Vision/MultiTagDevsY", this.frontLeftIO.getMultiTagStdDevsCoeff().get(1, 0));
+    multiTagStdDevTheta =
+        new LoggedTunableNumber(
+            "Vision/MultiTagDevsTheta", this.frontLeftIO.getMultiTagStdDevsCoeff().get(2, 0));
+  }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    frontLeftIO.updateInputs(frontLeftIOInputs);
+    Logger.processInputs("Vision/FrontLeft", frontLeftIOInputs);
+    frontRightIO.updateInputs(frontRightIOInputs);
+    Logger.processInputs("Vision/FrontRight", frontRightIOInputs);
+
+    if (Constants.debuggingMode) {
+      updateTunableNumbers();
+    }
+  }
+
+  /** Method to update tunable numbers */
+  private void updateTunableNumbers() {
+    LoggedTunableNumber.ifChanged(
+        hashCode(),
+        () -> {
+          frontLeftIO.setSingleTagStdDevs(
+              singleTagStdDevX.get(), singleTagStdDevY.get(), singleTagStdDevTheta.get());
+          frontRightIO.setSingleTagStdDevs(
+              singleTagStdDevX.get(), singleTagStdDevY.get(), singleTagStdDevTheta.get());
+        },
+        singleTagStdDevX,
+        singleTagStdDevY,
+        singleTagStdDevTheta);
+    LoggedTunableNumber.ifChanged(
+        hashCode(),
+        () -> {
+          frontLeftIO.setMultiTagStdDevs(
+              multiTagStdDevX.get(), multiTagStdDevY.get(), multiTagStdDevTheta.get());
+          frontRightIO.setMultiTagStdDevs(
+              multiTagStdDevX.get(), multiTagStdDevY.get(), multiTagStdDevTheta.get());
+        },
+        multiTagStdDevX,
+        multiTagStdDevY,
+        multiTagStdDevTheta);
   }
 }
