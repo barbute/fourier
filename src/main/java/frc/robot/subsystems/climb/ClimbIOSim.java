7165fb41ae9e26d5a7ecd8a89f80149c35f12238
barbute <endman3040@gmail.com>
2024-04-02 14:28:12 -0500

    Physics sim implementation of ClimbIO

--- src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
+++ src/main/java/frc/robot/subsystems/climb/ClimbIOSim.java
@@ -4,16 +4,66 @@
 
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
+
 /** Physics sim implementation of ClimbIO */
 public class ClimbIOSim implements ClimbIO {
+  private final double LOOP_PERIOD_SECS = 0.02;
   private final double GEAR_RATIO = 125.0 / 1.0;
 
-  @Override
-  public void updateInputs(ClimbIOInputs inputs) {}
+  private SingleJointedArmSim leftMotor =
+      new SingleJointedArmSim(
+          DCMotor.getNEO(1),
+          GEAR_RATIO,
+          0.005,
+          Units.inchesToMeters(12.5),
+          -Math.PI,
+          Math.PI,
+          true,
+          Math.PI);
+  private SingleJointedArmSim rightMotor =
+      new SingleJointedArmSim(
+          DCMotor.getNEO(1),
+          GEAR_RATIO,
+          0.005,
+          Units.inchesToMeters(12.5),
+          -Math.PI,
+          Math.PI,
+          true,
+          Math.PI);
+
+  private double leftAppliedVolts = 0.0;
+  private double rightAppliedVolts = 0.0;
 
   @Override
-  public void setLeftVolts(double volts) {}
+  public void updateInputs(ClimbIOInputs inputs) {
+    leftMotor.update(LOOP_PERIOD_SECS);
+    rightMotor.update(LOOP_PERIOD_SECS);
+
+    inputs.leftPosition = Rotation2d.fromRadians(leftMotor.getAngleRads());
+    inputs.leftAppliedVolts = leftAppliedVolts;
+    inputs.leftAppliedCurrentAmps = new double[] {leftMotor.getCurrentDrawAmps()};
+    inputs.leftTemperatureCelsius = new double[] {0.0};
+
+    inputs.rightPosition = Rotation2d.fromRadians(rightMotor.getAngleRads());
+    inputs.rightAppliedVolts = rightAppliedVolts;
+    inputs.rightAppliedCurrentAmps = new double[] {rightMotor.getCurrentDrawAmps()};
+    inputs.rightTemperatureCelsius = new double[] {0.0};
+  }
 
   @Override
-  public void setRightVolts(double volts) {}
+  public void setLeftVolts(double volts) {
+    leftAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+    leftMotor.setInputVoltage(leftAppliedVolts);
+  }
+
+  @Override
+  public void setRightVolts(double volts) {
+    rightAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+    rightMotor.setInputVoltage(rightAppliedVolts);
+  }
 }
