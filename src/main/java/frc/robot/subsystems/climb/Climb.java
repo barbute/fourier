f15dc0145def39d8a0381d699933a80638ca0c60
barbute <endman3040@gmail.com>
2024-04-02 14:51:05 -0500

    Added setpoints for Climb and ability to run it

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -4,11 +4,79 @@
 
 package frc.robot.subsystems.climb;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.util.function.DoubleSupplier;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
 public class Climb extends SubsystemBase {
-  public Climb() {}
+  public enum ClimbSetpoints {
+    BOTH_IN(() -> 12.0, () -> 12.0),
+    BOTH_OUT(() -> -12.0, () -> -12.0),
+    STOPPED(() -> 0.0, () -> 0.0);
+
+    private DoubleSupplier leftSetpointVolts;
+    private DoubleSupplier rightSetpointVolts;
+
+    ClimbSetpoints(DoubleSupplier leftVolts, DoubleSupplier rightVolts) {
+      this.leftSetpointVolts = leftVolts;
+      this.rightSetpointVolts = rightVolts;
+    }
+
+    public double getLeftVolts() {
+      return this.leftSetpointVolts.getAsDouble();
+    }
+
+    public double getRightVolts() {
+      return this.rightSetpointVolts.getAsDouble();
+    }
+  }
+
+  private ClimbSetpoints currentSetpoint = ClimbSetpoints.STOPPED;
+
+  private ClimbIO climbIO;
+  private ClimbIOInputsAutoLogged climbIOInputs = new ClimbIOInputsAutoLogged();
+
+  public Climb(ClimbIO io) {
+    climbIO = io;
+  }
 
   @Override
-  public void periodic() {}
+  public void periodic() {
+    climbIO.updateInputs(climbIOInputs);
+    Logger.processInputs("Climb", climbIOInputs);
+
+    if (DriverStation.isDisabled()) {
+      stop();
+    }
+
+    if (currentSetpoint != null) {
+      climbIO.setLeftVolts(currentSetpoint.getLeftVolts());
+      climbIO.setRightVolts(currentSetpoint.getRightVolts());
+    }
+  }
+
+  /** Sets the current setpoint to null and sets the volts to 0 */
+  public void stop() {
+    this.currentSetpoint = null;
+
+    climbIO.setLeftVolts(0.0);
+    climbIO.setRightVolts(0.0);
+  }
+
+  /** Run the climb at a setpoint voltage */
+  public void runClimb(ClimbSetpoints setpoint) {
+    currentSetpoint = setpoint;
+  }
+
+  /** Returns the current setpoint state of the Climb */
+  @AutoLogOutput(key = "Climb/CurrentSetpoint")
+  public ClimbSetpoints getCurrentSetpoint() {
+    if (currentSetpoint != null) {
+      return currentSetpoint;
+    } else {
+      return ClimbSetpoints.STOPPED;
+    }
+  }
 }
